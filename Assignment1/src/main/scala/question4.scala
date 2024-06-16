5186e788c6996ec8a0dd9134e7d35486491c42b5
SW33TSTUFF <simaakniyaz@gmail.com>
2024-06-16 11:19:51 +0530

    Fixed Assignment 1

--- Assignment1/src/main/scala/question4.scala
+++ Assignment1/src/main/scala/question4.scala
@@ -1,17 +1,15 @@
 object question4 extends App {
   val coverPrice: Double = 24.95
-  val bookCount: Int = 60
-  val discount: Float = 0.4
+  val bCount: Int = 60
+  val shipFirst: Double = 3
+  val shipAdditional: Double = 0.75
+  val discount: Double = 0.4
+  
+  val wholesalePrice = coverPrice * (1 - discount)
+  val totalCost = if (bCount < 50) (wholesalePrice + shipFirst * bCount)
+  	else ((wholesalePrice + shipFirst * 50) + (wholesalePrice + shipAdditional * (bCount - 50)))
 
-  def bookCost(coverPrice: Double, bookCount: Int, discount: Float): Double =
-    coverPrice * (1 - discount) * bookCount
-
-  def totalWholesaleCost(coverPrice: Double, bookCount: Int, discount: Float): Double =
-    if (bookCount <= 50) bookCost(coverPrice, bookCount, discount)
-    else bookCost(coverPrice, 50, discount) + bookCost(coverPrice, bookCount - 50, discount)
-
-  val wholesaleCost = totalWholesaleCost(coverPrice, bookCount, discount)
-  println(s"The total wholesale cost for $bookCount copies is Rs. $wholesaleCost")
-
-  // Need to double check the logic, same question as the one in the lecture slide
+	
+  println(s"The total wholesale cost for $bCount copies is Rs. $totalCost")
 }
+
