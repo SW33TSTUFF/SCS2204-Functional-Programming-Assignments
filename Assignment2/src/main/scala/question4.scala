5c3f57fc34d04c17ce675ec367fef33b66e09567
SW33TSTUFF <simaakniyaz@gmail.com>
2024-06-24 21:14:21 +0530

    Fixed logic

--- Assignment2/src/main/scala/question4.scala
+++ Assignment2/src/main/scala/question4.scala
@@ -2,6 +2,12 @@ object question4 extends App {
 
   val flatPerformancePrice = 500
 
+  def attendance(ticketPrice: Int):Int = {
+    var gradient = (120 - 100) / (15 - 20) // -1/4
+    var intercept = 120 + 15*4 // y = m(intercept) + c
+    gradient * ticketPrice + intercept
+  }
+
   def baseProfit(ticketPrice: Int, totalAttendance: Int): Int = {
     ticketPrice * totalAttendance
   }
@@ -10,18 +16,18 @@ object question4 extends App {
     3 * count
   }
 
-  def trueProfit(ticketPrice: Int, totalAttendance: Int): Int = {
-    baseProfit(ticketPrice, totalAttendance) - (costPerAttendee(totalAttendance) + flatPerformancePrice)
+  def trueProfit(ticketPrice: Int): Int = {
+    baseProfit(ticketPrice, attendance(ticketPrice)) - (costPerAttendee(attendance(ticketPrice)) + flatPerformancePrice)
   }
 
-  println("Profit at Rs. 5 : " + trueProfit(5, 160)) // cannot go any lower than 5
-  println("Profit at Rs. 10 : " + trueProfit(10, 140))
-  println("Profit at Rs. 15 : " + trueProfit(15, 120))
-  println("Profit at Rs. 20 : " + trueProfit(20, 100))
-  println("Profit at Rs. 25 : " + trueProfit(25, 80))
-  println("Profit at Rs. 30 : " + trueProfit(30, 60))
-  println("Profit at Rs. 35 : " + trueProfit(35, 40))
-  println("Profit at Rs. 40 : " + trueProfit(5, 20)) // cannot have attendance any less than 20
+  println("Profit at Rs. 5 : " + trueProfit(5)) // cannot go any lower than 5
+  println("Profit at Rs. 10 : " + trueProfit(10))
+  println("Profit at Rs. 15 : " + trueProfit(15))
+  println("Profit at Rs. 20 : " + trueProfit(20))
+  println("Profit at Rs. 25 : " + trueProfit(25))
+  println("Profit at Rs. 30 : " + trueProfit(30))
+  println("Profit at Rs. 35 : " + trueProfit(35))
+  println("Profit at Rs. 40 : " + trueProfit(40)) // cannot have attendance any less than 20
 
-  println("The best ticket price is Rs. 25 with a total profit of Rs. " + trueProfit(25,80))
+  println("The best ticket price is Rs. 25 with a total profit of Rs. " + trueProfit(25))
 }
