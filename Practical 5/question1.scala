20397a46850753cb972d4aa87f4737bd8329ae22
SW33TSTUFF <simaakniyaz@gmail.com>
2024-07-16 07:36:09 +0530

    Duplicate condition

--- Practical 5/question1.scala
+++ Practical 5/question1.scala
@@ -1,14 +1,19 @@
 import scala.io.StdIn
 
-object question1 {
+object question1 extends App{
 
   def getProductList(): List[String] = {
+    
     var list: List[String] = List()
     var str: String = ""
     print("Enter product = ")
     str = StdIn.readLine()
     while (str != "done") {
-      list = list :+ str
+      if (!list.contains(str)) {
+        list = list :+ str
+      } else {
+        println("Product already exists in the list.")
+      }
       print("Enter product = ")
       str = StdIn.readLine()
     }
@@ -25,10 +30,9 @@ object question1 {
     list.length
   }
 
-  def main(args: Array[String]): Unit = {
-    var list: List[String] = getProductList()
-    println("List")
-    printProductList(list)
-    println("Total number of products: " + getTotalProducts(list))
-  }
+  var list: List[String] = getProductList()
+  println("List")
+  printProductList(list)
+  println("Total number of products: " + getTotalProducts(list))
+
 }
